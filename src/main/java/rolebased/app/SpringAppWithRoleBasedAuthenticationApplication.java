a8212de964d97ffcc956f6217f2623541caaed93
sayed.hussainullah <sayedsadat344@gmail.com>
2023-05-15 09:56:54 +0430

    Initial commit

---
+++ src/main/java/rolebased/app/SpringAppWithRoleBasedAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package rolebased.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppWithRoleBasedAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppWithRoleBasedAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rolebased/app/SpringAppWithRoleBasedAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package rolebased.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppWithRoleBasedAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
